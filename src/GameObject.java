4a02d42ef512358e01a2642d21d39ecc2da65988
EpicProgrammer207 <league@WTS20.attlocal.net>
2017-08-14 19:57:03 -0700

    Accidentally forgot to save GameObject Class

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,15 +1,17 @@
 import java.awt.Graphics;
 
 public class GameObject {
-int x;
-int y;
-int width;
-int height;
-void update() {
-	
-}
-void draw(Graphics g) {
+	int x;
+	int y;
+	int width;
+	int height;
 
-	g.fillRect(x, y, 100, 100);
-}
+	void update() {
+
+	}
+
+	void draw(Graphics g) {
+
+		g.fillRect(x, y, 100, 100);
+	}
 }
